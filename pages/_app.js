c3f33687fbf7e492f0b609b2eff0cd427a49ec7a
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2022-04-02 11:55:53 +0200

    capitalized folder name

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/styles.css'
-import Layout from '../components/layout/layout';
+import Layout from '../components/Layout/layout';
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
