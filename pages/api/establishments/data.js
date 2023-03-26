9f727009f68aebdd0ef33be23980ce768a623f37
gokul <gokul465eswaran@gmail.com>
2023-03-26 22:55:55 +0530

    pic changes addeddate

--- pages/api/establishments/data.js
+++ pages/api/establishments/data.js
@@ -22,6 +22,50 @@ export const establishments = [
     stars: "five",
   },
   {
+    id: "01",
+    name: "Caravela Beach Resort Goa",
+    type: "hotel",
+    iconOne: "fa-solid fa-spa",
+    txtOne: "Goa",
+    iconTwo: "fa-solid fa-newspaper",
+    txtTwo: "Panjim , Goa",
+    email: "lorem@inn.com",
+    price: "3300",
+    maxGuests: "4",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/3e/52/e3/exterior.jpg?w=300&h=300&s=1",
+    descriptionShort: "Offering the 'Ginger Promise', Ginger Goa is a ...",
+    descriptionLong:
+      "Offering the 'Ginger Promise', Ginger Goa is a beautiful property with plenty of recreational options and comfortable facilities enriched by a contemporary ambience.",
+    address: "",
+    location: "Goa city center",
+    long: "",
+    lat: "",
+    stars: "five",
+  },
+  {
+    id: "01",
+    name: "Novotel Goa Resort & Spa    ",
+    type: "hotel",
+    iconOne: "fa-solid fa-spa",
+    txtOne: "Goa",
+    iconTwo: "fa-solid fa-newspaper",
+    txtTwo: "Panjim , Goa",
+    email: "lorem@inn.com",
+    price: "3300",
+    maxGuests: "4",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/db/57/ea/caravela-beach-resort.jpg?w=300&h=300&s=1",
+    descriptionShort: "Offering the 'Ginger Promise', Ginger Goa is a ...",
+    descriptionLong:
+      "Offering the 'Ginger Promise', Ginger Goa is a beautiful property with plenty of recreational options and comfortable facilities enriched by a contemporary ambience.",
+    address: "",
+    location: "Goa city center",
+    long: "",
+    lat: "",
+    stars: "five",
+  },
+  {
     id: "02",
     name: "Park Hyatt",
     type: "Hostel",
@@ -51,6 +95,36 @@ export const establishments = [
     ],
   },
   {
+    id: "02",
+    name: "GRT hotel",
+    type: "Hostel",
+    iconOne: "",
+    txtOne: "Chennai",
+    iconTwo: "",
+    txtTwo: "Velachery ,Chennai",
+    email: "ipsum@inn.com",
+    price: "4300",
+    maxGuests: "15",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/e2/ee/37/decent-pool.jpg?w=1200&h=-1&s=1",
+    descriptionShort:
+      "Park Hyatt Chennai is one of the luxury 5-star hotels on Velachery Road, near Raj Bhavan (Governor’s Residence) overlooking the Guindy National Park while Chennai International Airport is 15min away...",
+    descriptionLong:
+      "Park Hyatt Chennai is one of the luxury 5-star hotels on Velachery Road, near Raj Bhavan (Governor’s Residence) overlooking the Guindy National Park while Chennai International Airport is 15min away. IIT Madras, DLF IT Park, Tidel Park, Chennai Trade Centre, Marina beach, Apollo Proton Cancer Centre and American International School are within a 10km radius. Room Amenities",
+    address:
+      "39, Velachery Rd, near Raj Bhavan, Little Mount, Guindy, Chennai, Tamil Nadu 600032",
+    long: "",
+    lat: "",
+    stars: "four",
+    icons: [
+      "fa-solid fa-tv",
+      "fa-solid fa-shower",
+      "fa-solid fa-wheelchair-move",
+      "fa-solid fa-wifi",
+    ],
+  },
+
+  {
     id: "03",
     name: "Hotel Arafa Inn",
     type: "Hotel",
@@ -63,6 +137,50 @@ export const establishments = [
     maxGuests: "20",
     imgURL:
       "https://images.unsplash.com/photo-1445991842772-097fea258e7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    descriptionShort:
+      "Being located at the heart of the city, Arafa Inn is a perfect combination of strategic location and relaxed atmosphere. Arafa Inn is a stones throw away from the City Railway Station and Bangalore City Central Bus stand",
+    address: "",
+    descriptionLong:
+      "Being located at the heart of the city, Arafa Inn is a perfect combination of strategic location and relaxed atmosphere. Arafa Inn is a stones throw away from the City Railway Station and Bangalore City Central Bus stand. Arafa Inn, being centrally located is perfectly suited for tourists and business travelers alike. Arafa Inn is 15 minutes drive from all the major attractions of the city and is extremely close to the business district as-well.",
+    long: "",
+    lat: "",
+    stars: "three",
+  },
+  {
+    id: "03",
+    name: "The Leela palace",
+    type: "Hotel",
+    iconOne: "",
+    txtOne: "Bangalore",
+    iconTwo: "",
+    txtTwo: "Gandhi Nagar, Bangalore",
+    email: "",
+    price: "1900",
+    maxGuests: "20",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/ff/ee/27/exterior-day.jpg?w=1100&h=-1&s=1",
+    descriptionShort:
+      "Being located at the heart of the city, Arafa Inn is a perfect combination of strategic location and relaxed atmosphere. Arafa Inn is a stones throw away from the City Railway Station and Bangalore City Central Bus stand",
+    address: "",
+    descriptionLong:
+      "Being located at the heart of the city, Arafa Inn is a perfect combination of strategic location and relaxed atmosphere. Arafa Inn is a stones throw away from the City Railway Station and Bangalore City Central Bus stand. Arafa Inn, being centrally located is perfectly suited for tourists and business travelers alike. Arafa Inn is 15 minutes drive from all the major attractions of the city and is extremely close to the business district as-well.",
+    long: "",
+    lat: "",
+    stars: "three",
+  },
+  {
+    id: "03",
+    name: "Taj west end",
+    type: "Hotel",
+    iconOne: "",
+    txtOne: "Bangalore",
+    iconTwo: "",
+    txtTwo: "Gandhi Nagar, Bangalore",
+    email: "",
+    price: "1900",
+    maxGuests: "20",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/24/86/2e/lemon-tree-hotel-chennai.jpg?w=1100&h=-1&s=1",
     descriptionShort:
       "Being located at the heart of the city, Arafa Inn is a perfect combination of strategic location and relaxed atmosphere. Arafa Inn is a stones throw away from the City Railway Station and Bangalore City Central Bus stand",
     address: "",
@@ -95,8 +213,77 @@ export const establishments = [
     stars: "two",
   },
   {
+    id: "04",
+    name: "Hyaat Mumbai",
+    type: "Cabin",
+    iconOne: "",
+    txtOne: "Mumbai",
+    iconTwo: "",
+    txtTwo: "Goregaon",
+    email: "",
+    price: "4000",
+    maxGuests: "17",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/2b/6e/e3/pool.jpg?w=700&h=-1&s=1",
+    descriptionShort:
+      "Ginger Mumbai Goregaon is a new design hotel in the heart of Business district Goregaon in Mumbai. Strategically located off western express highway 1.6kms from the Railway Station Ginger Mumbai Goregaon",
+    address: "",
+    descriptionLong:
+      "Ginger Mumbai Goregaon is a new design hotel in the heart of Business district Goregaon in Mumbai. Strategically located off western express highway 1.6kms from the Railway Station Ginger Mumbai Goregaon is in close proximity to Mumbais International Airport and the Bombay Exhibition Center. The hotel also provides convenient access to the citys thriving corporate hub on the Andheri Kurla Road",
+    long: "",
+    lat: "",
+    stars: "two",
+  },
+  {
+    id: "04",
+    name: "Empire Royale Hotel",
+    type: "Cabin",
+    iconOne: "",
+    txtOne: "Mumbai",
+    iconTwo: "",
+    txtTwo: "Goregaon",
+    email: "",
+    price: "4000",
+    maxGuests: "17",
+    imgURL:
+      "https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    descriptionShort:
+      "Ginger Mumbai Goregaon is a new design hotel in the heart of Business district Goregaon in Mumbai. Strategically located off western express highway 1.6kms from the Railway Station Ginger Mumbai Goregaon",
+    address: "",
+    descriptionLong:
+      "Ginger Mumbai Goregaon is a new design hotel in the heart of Business district Goregaon in Mumbai. Strategically located off western express highway 1.6kms from the Railway Station Ginger Mumbai Goregaon is in close proximity to Mumbais International Airport and the Bombay Exhibition Center. The hotel also provides convenient access to the citys thriving corporate hub on the Andheri Kurla Road",
+    long: "",
+    lat: "",
+    stars: "two",
+  },
+  {
     id: "05",
-    name: "Hotel Regent Grand",
+    name: "Crown Plaza",
+    type: "Lodge",
+    iconOne: "",
+    txtOne: "Delhi",
+    iconTwo: "",
+    txtTwo: "Patel Nagar , Akshardham Temple",
+    email: "lorem@inn.com",
+    price: "3100",
+    maxGuests: "40",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/2b/6e/e3/pool.jpg?w=700&h=-1&s=1",
+
+    descriptionShort:
+      "Easily accessible from Jhandewalan Metro Station, this charming property offers cosy rooms, a stylish restaurant, a banquet hall and upscale facilities.",
+    descriptionLong:
+      "Easily accessible from Jhandewalan Metro Station, this charming property offers cosy rooms, a stylish restaurant, a banquet hall and upscale facilities.",
+    description: "",
+    address: "",
+    location: "Delhi city center",
+    long: "",
+    lat: "",
+    stars: "one",
+  },
+  {
+    id: "05",
+    name: "Hotel Regency",
     type: "Lodge",
     iconOne: "",
     txtOne: "Delhi",
@@ -119,8 +306,32 @@ export const establishments = [
     stars: "one",
   },
   {
+    id: "05",
+    name: "Vertu ",
+    type: "Lodge",
+    iconOne: "",
+    txtOne: "Delhi",
+    iconTwo: "",
+    txtTwo: "Patel Nagar , Akshardham Temple",
+    email: "lorem@inn.com",
+    price: "3100",
+    maxGuests: "40",
+    imgURL:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/ff/ee/27/exterior-day.jpg?w=1100&h=-1&s=1",
+    descriptionShort:
+      "Easily accessible from Jhandewalan Metro Station, this charming property offers cosy rooms, a stylish restaurant, a banquet hall and upscale facilities.",
+    descriptionLong:
+      "Easily accessible from Jhandewalan Metro Station, this charming property offers cosy rooms, a stylish restaurant, a banquet hall and upscale facilities.",
+    description: "",
+    address: "",
+    location: "Delhi city center",
+    long: "",
+    lat: "",
+    stars: "one",
+  },
+  {
     id: "06",
-    name: "Park Hyatt",
+    name: "GRT hotel",
     type: "Hostel",
     iconOne: "",
     txtOne: "Chennai",
@@ -130,7 +341,7 @@ export const establishments = [
     price: "5000",
     maxGuests: "15",
     imgURL:
-      "https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      "https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
     descriptionShort:
       "Park Hyatt Chennai is one of the luxury 5-star hotels on Velachery Road, near Raj Bhavan (Governor’s Residence) overlooking the Guindy National Park while Chennai International Airport is 15min away...",
     descriptionLong:
